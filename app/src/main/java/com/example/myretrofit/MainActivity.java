893096f410923846fb76c9601cf974664acfbb98
Anilkumar8179 <anil8179kumar@gmail.com>
2021-11-14 11:26:50 +0530

    Sample Example Trying GitHub

---
+++ app/src/main/java/com/example/myretrofit/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myretrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
